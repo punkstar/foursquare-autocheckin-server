5c0dd2a1480ca58623b95c87f523a17f82071e25
Nick Jones <nick@nicksays.co.uk>
2014-03-12 22:28:50 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foursq::Application.config.secret_key_base = '225e544c49e540de9da3b97302999bea71bed919d77cf7a74b8252dc8da769a106b929fe25faca9acffb3a08b7d901c874c79b84bc9b3ebb48e64b32d649c2de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foursq::Application.config.session_store :cookie_store, key: '_foursq_session'
